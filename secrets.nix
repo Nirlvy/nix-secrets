35619dae0dfac0cd355d319fdd58187700bf9322
Nirlvy <nirlvy@qq.com>
2024-08-18 15:24:11 +0800

    add sub.age

---
+++ secrets.nix
@@ -0,0 +1,11 @@
+let
+  nirlvy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHu883l55ki3mgiDfGRmN4H/3Pi1NXQkBAgCsvLhhvmH nirlvy@NullPointer";
+
+  NullPointer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+oInk8j7ldDXpzhmsN2xGFcMdAuOOT7fz+k7Knr+vD root@NullPointer";
+
+  users = [ nirlvy ];
+  systems = [ NullPointer ];
+in
+{
+  "./sub.age".publicKeys = users ++ systems;
+}
