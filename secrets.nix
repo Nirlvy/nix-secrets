0b1b57c9deb9dd41013666f72060507878d6b6ec
Nirlvy <nirlvy@outlook.com>
2025-06-14 21:41:40 +0800

    add wsl ssh key

--- secrets.nix
+++ secrets.nix
@@ -1,10 +1,18 @@
 let
   nirlvy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHu883l55ki3mgiDfGRmN4H/3Pi1NXQkBAgCsvLhhvmH nirlvy@NullPointer";
+  nirlvy-wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJATGVjUaVha5Isd3ErOgRaXF6g/tnVfFxGtRNMEjXXH nirlvy@outlook.com";
 
   NullPointer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+oInk8j7ldDXpzhmsN2xGFcMdAuOOT7fz+k7Knr+vD root@NullPointer";
+  WSL = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9Uu0/Kx8Pw/05/FxFsXlRqqLgv7Z85vtskRg4LIQOt root@WSL";
 
-  users = [ nirlvy ];
-  systems = [ NullPointer ];
+  users = [
+    nirlvy
+    nirlvy-wsl
+  ];
+  systems = [
+    NullPointer
+    WSL
+  ];
 in
 {
   "passwd.age".publicKeys = users ++ systems;
